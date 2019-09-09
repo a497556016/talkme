6ac4e6d71ebde41943c1e0edb5a73d1b5a51c724
heshaowei <heshaowei@huacenter.com>
2019-09-09 14:08:54 +0800

    文件缓存同步问题

--- client/app-only/src/store/file/index.js
+++ client/app-only/src/store/file/index.js
@@ -3,6 +3,21 @@ import {file as types} from "../types"
 
 const FILE_CACHE_DATA = "FILE_CACHE_DATA";
 
+//获取缓存数据
+const getCache = function () {
+    const json = localStorage.getItem(FILE_CACHE_DATA);
+    if(json){
+        return JSON.parse(json);
+    }
+    return {};
+}
+//更新缓存
+const updateCache = function (key, data) {
+    const cache = getCache();
+    cache[key] = data;
+    localStorage.setItem(FILE_CACHE_DATA, JSON.stringify(cache));
+}
+
 const state = {}
 
 const getters = {
@@ -17,23 +32,15 @@ const mutations = {
 
 const actions = {
     async [types.GET_BASE64_FILE]({state}, path){
-        let data, cacheData = {};
-
         //从缓存取
-        const json = localStorage.getItem(FILE_CACHE_DATA);
-        if(json){
-            cacheData = JSON.parse(json);
-            if(cacheData){
-                data = cacheData[path];
-            }
-        }
+        let data = getCache()[path]
 
         //缓存没有，去后台下载后缓存
         if(!data){
+            console.log(path);
             data = await api.base64(path);
 
-            cacheData[path] = data;
-            localStorage.setItem(FILE_CACHE_DATA, JSON.stringify(cacheData));
+            updateCache(path, data);
         }
 
         return data;
