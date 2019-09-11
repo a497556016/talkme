3663f590f73693754dddd92a5210aa06d76e8db5
heshaowei <heshaowei@huacenter.com>
2019-09-11 13:41:30 +0800

    语音延后加载

--- client/app-only/src/components/chat/MsgBox.vue
+++ client/app-only/src/components/chat/MsgBox.vue
@@ -21,7 +21,7 @@
                 </span>
 
                 <span style="margin-left: 20px;font-size: 12px">{{audioCurrentTime}}/{{audioDuration}} s</span>
-                <audio ref="audio" :src="fullPath(record.src)" @canplay="audioReadyPlay"></audio>
+<!--                <audio ref="audio" :src="fullPath(record.src)" @canplay="audioReadyPlay"></audio>-->
             </div>
             <div class="picture" v-else-if="record.mediaType == 'VIDEO'">
                 <img :src="fullPath(record.thumbnail)">
@@ -96,29 +96,40 @@
             fullScreenView(path){
                 this.$imageViewer(this.fullPath(path));
             },
-            audioReadyPlay(){
+            /*audioReadyPlay(){
                 this.audio = this.$refs['audio'];
 
                 this.audioCurrentTime = this.audio.currentTime;
                 this.audioDuration = this.audio.duration.toFixed(2);
+            },*/
+            execRecord(){
+                if(!this.play){
+                    this.audio.pause();
+                }else {
+                    this.audio.play();
+                    const i = setInterval(() => {
+                        if(this.audio.ended || !this.play){
+                            this.play = false;
+                            clearInterval(i);
+                        }
+                        this.audioCurrentTime = this.audio.currentTime.toFixed(1);
+                    }, 100);
+                }
             },
             playAudio(){
-                if(this.audio) {
-                    console.log(this.audio.currentTime)
-                    if(this.play){
-                        this.audio.pause();
-                        this.play = false;
-                    }else {
-                        this.audio.play();
-                        this.play = true;
-                        const i = setInterval(() => {
-                            if(this.audio.ended || !this.play){
-                                this.play = false;
-                                clearInterval(i);
-                            }
-                            this.audioCurrentTime = this.audio.currentTime.toFixed(2);
-                        }, 1000);
+                this.play = !this.play;
+                if(!this.audio) {
+                    this.audio = document.createElement('audio');
+                    this.audio.src = this.fullPath(this.record.src);
+                    this.audio.oncanplay = () => {
+                        console.log('准备好播放')
+                        this.audioCurrentTime = this.audio.currentTime;
+                        this.audioDuration = this.audio.duration.toFixed(1);
+
+                        this.execRecord();
                     }
+                }else {
+                    this.execRecord();
                 }
             },
             stopAudio(){
@@ -137,7 +148,7 @@
 </script>
 
 <style scoped lang="less">
-
+    @import "../../assets/style/color";
     .msg {
         .avatar {
             width: 36px;
@@ -160,19 +171,12 @@
                 line-height: 20px;
             }
             .text {
-                /*width: 100%;*/
                 border: 0px solid #d2dde6;
                 border-radius: 5px;
-
-                color: #e7f3fd;
-                /*height: 100%;*/
-                /*width: fit-content;*/
                 padding: 0 5px;
-                /*max-width: 70%;*/
-                /*display: table;*/
                 position: relative;
-
                 white-space:pre-wrap;
+                color: @black-native;
             }
             .picture {
                 width: 100%;
@@ -196,8 +200,10 @@
             .audio {
                 border-radius: 5px;
                 padding: 0 10px;
-                font-size: 25px;
+                font-size: 23px;
                 display: flex;
+                position: relative;
+                color: @black-native;
             }
 
             .video {
@@ -224,13 +230,24 @@
             }
             .text {
                 background: #fff;
-                color: #000;
+                /*color: #000;*/
             }
             .audio {
                 background: #fff;
-                color: #000;
+                /*color: #000;*/
             }
             .text:before{
+                position: absolute;
+                content: "";
+                width: 0;
+                height: 0;
+                right: 100%;
+                top: 8px;
+                border-top: 6px solid transparent;
+                border-right: 10px solid #fff;
+                border-bottom: 6px solid transparent;
+            }
+            .audio:before{
                 position: absolute;
                 content: "";
                 width: 0;
@@ -258,12 +275,25 @@
                 text-align: right;
             }
             .text {
-                background: #3fcb6e;
+                background: #5fec8f;
+                /*color: #e7f3fd;*/
             }
             .audio {
-                background: #3fcb6e;
+                background: #5fec8f;
+                /*color: #e7f3fd;*/
             }
             .text:after{
+                position: absolute;
+                content: "";
+                width: 0;
+                height: 0;
+                left: 100%;
+                top: 8px;
+                border-top: 6px solid transparent;
+                border-left: 10px solid #3fcb6e;
+                border-bottom: 6px solid transparent;
+            }
+            .audio:after{
                 position: absolute;
                 content: "";
                 width: 0;
